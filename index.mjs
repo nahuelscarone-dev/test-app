505b89976755c51245e32bd6080537c203d5821d
nahuelscarone-dev <nahuelscarone@gmail.com>
2026-06-17 12:39:40 -0300

    feat: crear conexión a Supabase

--- index.mjs
+++ index.mjs
@@ -1,5 +1,18 @@
+import 'dotenv/config'
 import express from "express"
 import { Pool } from "pg"
+
+//////////////////////////
+// SUPABASE -> click console / librería
+const pool = new Pool({
+    user:process.env.BD_HOST,
+    password: process.env.BD_PASS,
+    user: process.env.BD_USER,
+    database: process.env.BD_NAME,
+    port: process.env.BD_PORT
+})
+//////////////////////////
+
 
 const PUERTO = process.env.PORT || 3000
 const app = express()
@@ -7,6 +20,11 @@ const app = express()
 
 app.get('/test', (req, res)=>{
  res.send('Test funcionando')
+})
+
+app.get('/test-bd', async (req, res)=>{
+ const resultado = await pool.query('SELECT * FROM MENSAJES')
+ res.json(resultado.rows)
 })
 
 app.listen(PUERTO,()=>{
